8724183c8eded434382c4c2870c43f491aab0de6
Lee Heap <lee@lheap.co.uk>
2023-08-16 10:38:32 +0100

    Autocommit from 2023-08-16 10:38:32

---
+++ voron/Firmware/Canboot/OctopusF446/src/stm32/chipid.d
@@ -0,0 +1,8 @@
+out/src/stm32/chipid.o: src/stm32/chipid.c src/generic/canserial.h \
+ /usr/lib/gcc/arm-none-eabi/8.3.1/include/stdint.h src/generic/usb_cdc.h \
+ src/generic/usbstd.h src/compiler.h src/stm32/internal.h out/autoconf.h \
+ lib/stm32f4/include/stm32f4xx.h lib/stm32f4/include/stm32f446xx.h \
+ lib/cmsis-core/core_cm4.h lib/cmsis-core/cmsis_version.h \
+ lib/cmsis-core/cmsis_compiler.h lib/cmsis-core/cmsis_gcc.h \
+ lib/cmsis-core/mpu_armv7.h lib/stm32f4/include/system_stm32f4xx.h \
+ src/sched.h src/ctr.h src/compiler.h
